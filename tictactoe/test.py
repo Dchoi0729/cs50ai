003bc49ac56b2b669bd506435744a01e30a31259
Dchoi0729 <yc3878@columbia.edu>
2022-10-01 03:29:39 +0000

    submit tictactoe

--- tictactoe/test.py
+++ tictactoe/test.py
@@ -1,20 +1,14 @@
 import tictactoe
-from copy import deepcopy
-# import the builtin time module
 import time
 
 X = "X"
 O = "O"
 EMPTY = None
 
-
 board = [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
         
-
-
-
 # Grab Currrent Time Before Running the Code
 start = time.time()
 
@@ -26,24 +20,3 @@ end = time.time()
 #Subtract Start Time from The End Time
 total_time = end - start
 print("\n"+ str(total_time))
-
-'''
-print(tictactoe.player(board))
-
-print(len(board))
-print(len(board[0]))
-
-print(tictactoe.actions(board))
-
-
-
-print(board)
-
-#print(tictactoe.result(board, (0,3)))
-
-
-print(tictactoe.winner(board))
-print(tictactoe.terminal(board))
-print(tictactoe.utility(board))
-print(tictactoe.minimax(board))
-'''
